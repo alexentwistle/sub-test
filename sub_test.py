26ef097190570baff1f48eab2e1edba6ddd85939
alexentwistle <alexentwistleseo@gmail.com>
2021-05-29 23:31:50 +0100

    merged check_status.py into sub_test.py

--- sub_test.py
+++ sub_test.py
@@ -1,10 +1,11 @@
 import sublist3r
+import requests
 
 # Uncomment below to allow users to specify their own domain
 # domain = input("Specify the domain: ")
 
-# Placeholder. TODO: Remove
-domain = "example.org"
+# Placeholder. TODO: Replace with user input
+domain = "filmthreat.com"
 subdomains = sublist3r.main(domain, 40, 'discovered.txt', ports= None, silent=True, verbose= False, enable_bruteforce= False, engines=None)
 
 with open('discovered.txt') as infile, open('fixed.txt', 'w') as outfile:
@@ -12,3 +13,14 @@ with open('discovered.txt') as infile, open('fixed.txt', 'w') as outfile:
         if not line.strip(): continue  # Skip empty lines
         outfile.write("http://" + line)  # Write non-empty lines to output. Once with HTTP protocol...
         outfile.write("https://" + line) # ... and once with HTTPS protocol
+
+with open('fixed.txt') as infile, open('results.txt', 'w') as outfile:
+    for url in infile:
+        try:
+            clean_url = url.rstrip()
+            r = requests.get(clean_url, timeout=2)
+#           r = requests.get(clean_url, headers={'Connection': 'close'})
+#           r.encoding = 'utf-8'
+            outfile.write(clean_url+": "+str(r.status_code)+"\n")
+        except requests.ConnectionError as e:
+            outfile.write(clean_url+": Connection Error."+"\n")
